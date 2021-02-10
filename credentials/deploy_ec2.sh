a6e91ef671d86921d34099bbbc7d8d7ce2a90a64
pachecosf <austinwim@gmail.com>
2021-02-09 22:53:57 -0800

    update to bigger EC2 instance

--- credentials/deploy_ec2.sh
+++ credentials/deploy_ec2.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 echo 'Running deployment'
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@ec2-3-134-92-250.us-east-2.compute.amazonaws.com " sudo docker image prune -f 
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@ec2-3-17-191-42.us-east-2.compute.amazonaws.com " sudo docker image prune -f 
         cd csc648-02-sp21-team-05/application/docker-compose
         sudo docker-compose down
-        git checkout master
-        origin=$(git remote get-url origin)
-        origin_with_pass=${origin/"//"/"//${USER_ID}:${USER_PW}@"}
-        git pull
+        sudo git pull
         echo 'Pulling newer docker images'
         sudo docker-compose pull
         sudo docker-compose up -d
