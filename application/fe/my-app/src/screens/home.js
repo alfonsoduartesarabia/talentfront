a6da37065d6398db8e045fb50dbd42e6fcf5620c
Battulga Tsogtgerel <theonlytulga@gmail.com>
2021-03-14 19:25:27 -0700

    checkpoint

--- application/fe/my-app/src/screens/home.js
+++ application/fe/my-app/src/screens/home.js
@@ -1,1 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./home.css"
+import axios from 'axios'
+
+export default function (props) {
+  const [searchString, setSearchString] = useState("empty");
+  const [searchResult, setSearchResult] = useState([]);
+  const handleSubmit = () => {
+    console.log("MAKING API CALL with this DATA", searchString)
+    let data = {
+      "filter": "",
+      "subFilter": "",
+      "searchTerm": searchString
+    }
+    axios.post('/GET_IT_FROM_AUSTIN', data)
+    .then(function (response) {
+      console.log(response);
+      // setSearchResult(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  return (
+    <div className="searchbar-container">
+      HOME
+      <div className="searchbar-options-container">
+        <input onChange={ (e) => { setSearchString(e.target.value) } }/>
+        <div>
+          <input type="radio" value="jobPosting" />Job Posting
+          <input type="radio" value="talent"/>Talent
+          <input type="radio" value="professor"/>Professor
+          <input type="radio" value="organization"/>Organization  
+        </div>
+        
+      </div>
+      
+
+      <button onClick={handleSubmit}>
+        Search
+      </button>
+    </div>
+  )
+}
+
+/*
+{
+    "filter": "",
+    "subFilter": "",
+    "searchTerm": ""
+}
+
+{
+    "filterTypes": [
+        "", -> any
+        "jobPosting",
+        "talent",
+        "professor",
+        "organization"
+    ],
+    "subFilterTypes": [
+        "talent": [
+            "undergrad",
+            "graduate",
+            "postgrad"
+        ]
+    ]
+}
+*/
--- application/fe/my-app/src/screens/notes.js
+++ application/fe/my-app/src/screens/notes.js
@@ -1,34 +1,34 @@
 
-sync
-LINE EXECUTES
-LINE 2
+// sync
+// LINE EXECUTES
+// LINE 2
 
-async
-LINE FIRES => 2 secs => USER_DATA
-
-
-LINE DOESNT WAIT =>
+// async
+// LINE FIRES => 2 secs => USER_DATA
 
 
-CALLBACK
-api.get("https:/8321.3.23.2/user/data", function(data){
-  console.log(data.user);
-  data
-})
+// LINE DOESNT WAIT =>
 
 
-axios.get("https:/8321.3.23.2/user/data", function(data))
+// CALLBACK
+// api.get("https:/8321.3.23.2/user/data", function(data){
+//   console.log(data.user);
+//   data
+// })
 
-axios.get("https:/8321.3.23.2/user/data").then().then().catch()
+
+// axios.get("https:/8321.3.23.2/user/data", function(data))
+
+// axios.get("https:/8321.3.23.2/user/data").then().then().catch()
 
 
-PROMISE
-api.get("https:/8321.3.23.2/user/data")
-  .then( (data) => {
-    // display user data.
-  })
-  .catch( (err) => {
-    console.log(err)
-    // display error to user within UI
-  })
+// PROMISE
+// api.get("https:/8321.3.23.2/user/data")
+//   .then( (data) => {
+//     // display user data.
+//   })
+//   .catch( (err) => {
+//     console.log(err)
+//     // display error to user within UI
+//   })
 
