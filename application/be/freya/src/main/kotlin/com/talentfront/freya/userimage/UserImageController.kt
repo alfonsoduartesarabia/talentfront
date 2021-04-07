aca2ec10d0dd39f945b70f43a3dbb654aa6aeaa7
pachecosf <awimberley@sofi.org>
2021-04-06 19:40:46 -0700

    minor error in the jar

--- application/be/freya/src/main/kotlin/com/talentfront/freya/userimage/UserImageController.kt
+++ application/be/freya/src/main/kotlin/com/talentfront/freya/userimage/UserImageController.kt
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import java.io.File
 
 @RestController
 class UserImageController(
@@ -49,7 +48,7 @@ class UserImageController(
             }
             return ResponseEntity.ok().contentType(mediaType).body(data)
         }
-        val defaultImage = File(javaClass.getResource("/x.png").file).readBytes()
+        val defaultImage = javaClass.getClassLoader().getResourceAsStream("x.png")!!.readBytes()
         return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(defaultImage)
     }
 }
