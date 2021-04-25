a0b8d31f023b9e8c7c82f27164c27248b26c7694
Battulga Tsogtgerel <theonlytulga@gmail.com>
2021-04-25 16:09:47 -0700

    removed searchar.css error

--- application/fe/my-app/src/components/searchbar.js
+++ application/fe/my-app/src/components/searchbar.js
@@ -1,6 +1,5 @@
 // import { useState } from "react";
 import "./searchbar.sass";
-import "./searchbar.css";
 import { BsSearch } from "react-icons/bs";
 import { Link } from "react-router-dom";
 // import { Menu, Transition} from "@headlessui/react";
@@ -9,64 +8,55 @@ import { Link } from "react-router-dom";
 // import Footer from '../components/footer';
 
 const SearchBar = (props) => {
-
-    return (
-        <div id="nav">
-        <div className="navbar-search">
-
-          <div className="search-bar-wrapper">
-            <form action="" className="">
-              <div className="search-bar">
-                <input
+  return (
+    <div id="nav">
+      <div className="navbar-search">
+        <div className="search-bar-wrapper">
+          <form action="" className="">
+            <div className="search-bar">
+              <input
                 //   onChange={(event) => setSearchTerm(event.target.value)}
-                  type="text"
-                />
-                
-                <button className="search-btn">
-                  <div className="search-logo"><BsSearch />{""}</div>
-                </button>
-              </div>
+                type="text"
+              />
 
-              {/* <button  className="navbar-btn">
+              <button className="search-btn">
+                <div className="search-logo">
+                  <BsSearch />
+                  {""}
+                </div>
+              </button>
+            </div>
+
+            {/* <button  className="navbar-btn">
                 Search
               </button> */}
 
-                <div className="searchbar-options-container">
-                <label htmlFor="user-type">
-                </label>
-                    <select
-                    defaultValue=""
-                    name="search"
-                    id="user-type">
-                        <option value="">
-                            Filter
-                        </option>
-                        <option value="jobPosting">Job Posting</option> 
-                        <option value="talent">Talent</option>
-                        <option value="professor">Professor</option>
-                        <option value="organization">Organization</option>
-                    </select>
-
-                    
-            </div> 
-            </form>
-          </div>
-            <div className="navbar-right">
-            <Link to="/profile">
-            <button className="navbar-btn">Profile</button>
-            </Link>
-            {/* {loginButton} */}
-            <Link to="/login">
-            <button className="navbar-btn">Login</button>
-            </Link>
+            <div className="searchbar-options-container">
+              <label htmlFor="user-type"></label>
+              <select defaultValue="" name="search" id="user-type">
+                <option value="">Filter</option>
+                <option value="jobPosting">Job Posting</option>
+                <option value="talent">Talent</option>
+                <option value="professor">Professor</option>
+                <option value="organization">Organization</option>
+              </select>
             </div>
-          </div>
+          </form>
+        </div>
+        <div className="navbar-right">
+          <Link to="/profile">
+            <button className="navbar-btn">Profile</button>
+          </Link>
+          {/* {loginButton} */}
+          <Link to="/login">
+            <button className="navbar-btn">Login</button>
+          </Link>
+        </div>
+      </div>
 
-          {/* <Footer/> */}
-          </div>
-      );
-      
-      
-}
+      {/* <Footer/> */}
+    </div>
+  );
+};
 
 export default SearchBar;
